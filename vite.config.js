af7ab7b1010cd1314371939eda96dca9ff63e3e4
IRVING JONES VALDES MACIEL <irjovama@gmail.com>
2023-05-12 06:47:08 -0600

    add

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as dotenv from 'dotenv';
-dotenv.config();
-const port = process.env.PORT || 3000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: port
-  }
+  plugins: [react()]
 })
