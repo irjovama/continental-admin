f3b1f19fa916e1dde54d4ebb6dd12d26fe155a45
IRVING JONES VALDES MACIEL <irjovama@gmail.com>
2023-05-17 13:30:55 -0600

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="invitations/:token" element={<HomeInvitations />}></Route>
         <Route path="*" element={<Link to={"admin-panel/73e647e1-cfa3-4ade-9985-c44e4527a0d4"}>Link</Link>}></Route>
         <Route path="thanks" element={<Thanks />}></Route>
         <Route path="admin-panel/:tests_id" element={<AdminPanel />}></Route>
-        <Route path="invitations/:token" element={<HomeInvitations />}></Route>
         <Route
           path="invitations/:token/info"
           element={<InfoInvitations />}
